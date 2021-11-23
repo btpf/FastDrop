eb0cb735835d573be913a80a536a77e8e2f6f53b
Mohammad5566 <mohammadmahmud5566@yahoo.com>
2021-11-22 21:49:41 -0500

    working on navbar

--- src/web-app/src/index.js
+++ src/web-app/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App.js";
 
 ReactDOM.render(
-    <App/>,
+    <React.StrictMode><App /></React.StrictMode>,
   document.getElementById('root')
 );
