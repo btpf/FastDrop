eacd5e5d44bd5acd4254853089ff0b606f396760
Mohammad5566 <mohammadmahmud5566@yahoo.com>
2021-12-11 15:33:34 -0500

    Added box for files transferred on Home Page

--- src/web-app/src/components/Home.js
+++ src/web-app/src/components/Home.js
@@ -1,6 +1,6 @@
 import { useState, useReducer } from "react";
 import {
-    Center, Text, useColorModeValue,
+    Center, Text, useColorModeValue, Box, Spacer,
     VStack, Progress, HStack, StackDivider
 } from "@chakra-ui/react";
 
@@ -9,6 +9,7 @@ import libFastDrop from '../../../lib/libfastdrop'
 const lib = libFastDrop.getInstance();
 
 function Home() {
+
     const [ files, setFiles ] = useState(lib.getFileTransfers());
     const [ ignored, forceUpdate ] = useReducer(x => x + 1, 0);
 
@@ -20,21 +21,23 @@ function Home() {
     }
 
     return <Center>
-        <VStack pt="150px" spacing="24">
+        <VStack pt="100px" spacing="24">
             <Text fontWeight="bold" fontSize="26px"
                 color={useColorModeValue("gray.600", "gray.300")}>
-                Waiting for files...
+                {files.length === 0 ? "Waiting for files..." : "Files Shared"}
             </Text>
 
-            <VStack divider={<StackDivider/>} spacing="8">
-                {files.map((f, index) => (
-                    <HStack key={index}>
-                        <Text> {f.fileName}</Text>
-                        <Progress w="300px" h="10px" rounded="xl"
-                            value={(f.currentChunk / f.chunks) * 100}/>
-                    </HStack>
-                ))}
-            </VStack>
+            <Box rounded="lg" p="50px" bgColor={useColorModeValue("gray.300", "gray.700")}>
+                <VStack divider={<StackDivider/>} spacing="8">
+                    {files.map((f, index) => (
+                        <HStack key={index} w="700px" justify="space-between">
+                            <Text maxW="350px" > {f.fileName}</Text>
+                            <Progress w="250px" h="10px" rounded="xl"
+                                value={(f.currentChunk / f.chunks) * 100}/>
+                        </HStack>
+                    ))}
+                </VStack>
+            </Box>
         </VStack>
     </Center>
 }
--- src/web-app/src/components/Profile.js
+++ src/web-app/src/components/Profile.js
@@ -1,21 +1,23 @@
-import { Button, Center, Image, Text, useColorModeValue, VStack } from "@chakra-ui/react";
+import { Button, Center, Image, Text, useColorModeValue, VStack, Flex } from "@chakra-ui/react";
 
 function Profile( { files } ) {
     return <Center>
         <VStack pt="50px" spacing="8">
             <Text fontWeight="bold" fontSize="26px"
                   color={useColorModeValue("gray.600", "gray.300")}> My Name </Text>
-            <Image shadow="lg" h="13%" w="13%" rounded="2xl" alt="qr-code"
+            <Image shadow="lg" h={{base: "20%", md: "13%"}} w={{base: "20%", md: "13%"}}
+                   rounded="2xl" alt="qr-code"
                    src={useColorModeValue("https://i.ibb.co/Fnjh4v2/qr-code-1.png",
                        "https://i.ibb.co/VL7kpbS/qr-code.png")}/>
 
-            <VStack pt="40px" spacing="4">
-                <Text fontSize="xl" align="center" fontWeight="bold"> File Sharing History </Text>
-                {
-                    files.map((f, index) => (
-                        <Button> {f.fileName} </Button>
+            <Flex>
+                <VStack pt="40px" spacing="4" >
+                    <Text fontSize="xl" align="center" fontWeight="bold"> File Sharing History </Text>
+                    {files.map((f) => (
+                        <Button p="20px"> {f.fileName} </Button>
                     ))}
-            </VStack>
+                </VStack>
+            </Flex>
         </VStack>
     </Center>
 }
